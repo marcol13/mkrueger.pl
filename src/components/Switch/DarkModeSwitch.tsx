1ce51afd3172b2a20eaca1fda6c7c8dbc23da3f6
Marcin <marcinkrueger@gmail.com>
2023-12-31 14:59:11 +0100

    feat: add dark mode switch

---
+++ src/components/Switch/DarkModeSwitch.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import { Icon } from "../Icon/Icon";
+import { Button } from "../Button/Button";
+
+export const DarkModeSwitch = () => {
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(false)
+  //   localStorage.getItem("darkMode") !== null
+  //     ? JSON.parse(localStorage.getItem("darkMode") as string)
+  //     : false
+  // );
+  const [mounted, setMounted] = useState<boolean | null>();
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
+  }
+
+  return (
+    <Button
+      onClick={() => {
+        localStorage.setItem("darkMode", JSON.stringify(!isDarkMode));
+        setTheme(theme === "dark" ? "light" : "dark")
+        setIsDarkMode((prev) => !prev);
+      }}
+    >
+      <Icon variant={isDarkMode ? "moon" : "sun"} size="l" />
+    </Button>
+  );
+};
