d5201a7a226ce134d65fe1594030f9fa8f71f916
Marcin <marcinkrueger@onet.eu>
2024-03-27 17:59:16 +0100

    change pages folder name

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { About } from "@/pages/About/About";
-import { Portfolio } from "@/pages/Portfolio/Portfolio";
-import { Contact } from "@/pages/Contact/Contact";
-import { Experience } from "@/pages/Experience/Experience";
+import { About } from "@/views/About/About";
+import { Portfolio } from "@/views/Portfolio/Portfolio";
+import { Contact } from "@/views/Contact/Contact";
+import { Experience } from "@/views/Experience/Experience";
 
 export default function Home({
   params: { lang },
